ae48c09b6db2b9cb03ef76953ded2b8359ea9508
tiangong <tiangong@163.com>
2018-04-21 23:35:06 +0800

    init

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-$github_signal = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-list($hash_type,$hash_value)=explode('=',$github_signal,2);
-$payload=file_get_contents("php://input");
-$secret="xinghuaCOM";
-$hash=hash_hmac($hash_type,$payload,$secret);
-if($hash && $hash === $hash_value){
-	echo '认证成功,开始更新';
-	echo exec("sh github_auto_pull.sh");
-	echo date("Y-m-d H:i:s");
-}else{
-	echo '认证失败';
+$github_signal = $_SERVER['HTTP_X_HUB_SIGNATURE']; //获取github签名  
 
-}
+  list($hash_type,$hash_value) = explode('=', $github_signal,2);
+
+  //获取用户输入的
+  $payload = file_get_contents("php://input");
+  $secret = 'wenhaiCOM.';
+  //生成带有密钥的hash 值  
+  $hash = hash_hmac($hash_type, $payload, $secret);
+  if($hash && $hash === $hash_value){
+       echo '认证成功,开始更新';
+       echo exec("sh github_auto_pull.sh");
+       echo date('Y-m-d H:i:s');
+  }else{
+       echo '认证失败';
+  }
